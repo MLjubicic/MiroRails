be99f4776da868b8e060065ca6814b21a588ba33
Miro Ljubicic <miro@ljubicic.ch>
2012-02-28 21:24:48 +0100

    Applikation 01_VeranstaltungseinheitSetup\test2 erstellt und Kapitel 1&2 aus dem Buch durchgearbeitet -> alles OK

---
+++ 01_VeranstaltungseinheitSetup/test2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = '0c7e1d790b3df96ecdf11e0523e71ff1f20c499c2852bff642296bc37544b77c8ee2024a11a5781b6c5afc21c6887f732a23aae4a38dac1c1a27110467d6b890'
